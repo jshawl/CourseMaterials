65a27033c4eb6badeb1f2c647db8940320f3da15
Jesse Shawl <jesse@jshawl.com>
2015-03-17 21:07:14 -0400

    add some inclass things

---
+++ scratch/2015-03-17-control-flow.playground/section-1.swift
@@ -0,0 +1,92 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var sunny = false
+var funny = false
+var hairy = true
+
+if sunny {
+  println("The weather is great")
+}
+
+if !sunny {
+    println("Rainy as hell")
+}
+
+if sunny == false {
+  println("not sunny")
+}
+
+if funny || sunny{
+  println("funny or sunny")
+}
+
+if sunny {
+  println("sunny")
+} else {
+  println("not sunny")
+}
+
+if sunny {
+    println("sunny")
+} else if funny {
+    println("not sunny, but funny")
+} else {
+    println("neither sunny, nor funny")
+}
+
+var werthsAtBats:Int?
+var moreBats = werthsAtBats
+moreBats = nil
+var notOptional = 0
+//notOptional = nil
+
+println( moreBats )
+
+if moreBats != nil {
+  println("take a swing")
+}
+
+if let atBats = werthsAtBats {
+  println("\(atBats)")
+}
+
+var loopy = 30
+while loopy > 0 {
+  loopy = loopy - 1
+}
+
+do {
+  loopy = loopy + 1
+} while loopy != 30
+
+
+for var index = 0; index < 3; index++ {
+    println( index );
+}
+
+println(index) //wtf
+
+for index3 in 0..<5 {
+  println(index3)
+}
+
+//println(index3) but error
+
+for index3 in 0...5 {
+    println(index3)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+println("complete")
---
+++ scratch/2015-03-17-data-types.playground/section-1.swift
@@ -0,0 +1,42 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+println("Hello Class")
+
+let constantName = "pizza"
+
+println("Hello \(constantName)")
+
+var food = "Pizza!"
+
+println("I want some \(food)")
+
+let unix0 = 1970
+let cocoa0 = 2000
+
+33 % 6
+33 / 6
+
+var harpersAtBats = 255.0
+var harpersHits:Float = 100 // specifies type
+
+// casting
+var harpersBattingAverage = Double(harpersHits) / harpersAtBats
+
+var truncation = Int(0.51)
+
+
+var working = true
+var windy = false
+
+var pizza:String?
+
+pizza = "delish"
+
+println(pizza!)
+
+
+
+
+
